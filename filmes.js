e19eb8f191677a66183c08ee15805ed712a0de79
Althieres Dias <althierestm@gmail.com>
2026-09-08 12:05:40 -0300

    Create filmes.js

---
+++ filmes.js
@@ -0,0 +1,11 @@
+const filmesBackup = [
+  {
+    title: "Par Perfeito",
+    url: "http://fhd4.oneplayer.site/343rt342wtg34wetg34retg4rgh5kh4/FHD4/tt43640198.mp4",
+    poster: "https://i.imgur.com/L89LBAz.png", // Coloque o link da capa real aqui
+    desc: "A vida tranquila de Jen e Spencer começa a mudar quando ela descobre que o marido é um assassino profissional.",
+    rating: "14",
+    duration: "1h 40m",
+    genre: "Comédia/Ação"
+  }
+];
